b7b3ffcaad309909db18a99a4ab74a66336d2903
Brajesh825 <brajeshmishra825@gmail.com>
2024-07-03 01:04:59 +0530

    90% done

--- backend-lite/models/index.js
+++ backend-lite/models/index.js
@@ -1,23 +1,10 @@
 const sequelize = require('../sequelize');
 
 const User = require('./user.model');
-const Follower = require('./follower.model');
-const Following = require('./following.model');
 const Post = require("./post.model")
 const Like = require("./like.model")
 const Comment = require("./comment.model")
 
-// Establish relationships
-User.hasMany(Follower, { foreignKey: 'userId' });
-User.hasMany(Follower, { foreignKey: 'followerId' });
-User.hasMany(Following, { foreignKey: 'userId' });
-User.hasMany(Following, { foreignKey: 'followingId' });
-
-Follower.belongsTo(User, { foreignKey: 'userId' });
-Follower.belongsTo(User, { foreignKey: 'followerId' });
-
-Following.belongsTo(User, { foreignKey: 'userId' });
-Following.belongsTo(User, { foreignKey: 'followingId' });
 
 
 // Sync the models with the database
@@ -31,8 +18,6 @@ sequelize.sync()
 
 module.exports = { 
     User,
-    Follower, 
-    Following , 
     Post , 
     Like ,
     Comment
